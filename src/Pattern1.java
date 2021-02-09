4d89249165bb910aa0a08df8a8852722a6a533e1
Balaji250692 <anigalanblue@gmail.com>
2021-02-09 07:23:06 -0800

    changed

--- src/Pattern1.java
+++ src/Pattern1.java
@@ -4,10 +4,17 @@ public class Pattern1 {
 public static void main(String[] args) {
 	for (int i = 1; i <=5; i++) {
 		for (int j = 1; j <=i; j++) {
+			System.out.println("Balaji");
+			System.out.println("anand");
+			System.out.println("3");
+			System.out.println("2");
+			System.out.println("Area");
 			
 			System.out.print("*");
 		}
 		System.out.println("");
 	}
+	System.out.println("avhg");
 }
+
 }
