47c8d2a363effac095161d7f5369eb3447e6d97f
Raguselvaraj05 <raguashwin851@gmail.com>
2021-02-09 20:47:10 +0530

    Done

--- src/Pattern1.java
+++ src/Pattern1.java
@@ -4,7 +4,7 @@ public class Pattern1 {
 public static void main(String[] args) {
 	for (int i = 1; i <=5; i++) {
 		for (int j = 1; j <=i; j++) {
-			
+			System.out.println("I entered first");
 			System.out.print("*");
 		}
 		System.out.println("");
