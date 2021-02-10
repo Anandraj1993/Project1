90ee4e8ee51a6c4f26bb09ebdc3238e9cf3d4fc8
Raguselvaraj05 <raguashwin851@gmail.com>
2021-02-10 10:29:13 +0530

    Done

--- src/Pattern1.java
+++ src/Pattern1.java
@@ -11,6 +11,13 @@ public static void main(String[] args) {
 			System.out.println("2");
 			System.out.println("Area");
 			
+			
+			System.out.println("Shall we start a new project");
+			System.out.println("Anand");
+			System.out.println("Xavier");
+			System.out.println("Balaji");
+			System.out.println("Ragu");
+			System.out.println("Assemble by 8 o clock");
 
 			System.out.print("*");
 		}
